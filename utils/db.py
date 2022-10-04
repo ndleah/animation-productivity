051843961b755789b5a98f8b50ae7e1ed6cfa207
Leah Nguyen <nduongthucanh@gmail.com>
2022-10-04 23:19:45 +1100

    update
    update files as independent variables

---
+++ utils/db.py
@@ -0,0 +1,19 @@
+import snowflake.connector
+import streamlit as st
+
+@st.experimental_singleton
+def init_connection():
+  return snowflake.connector.connect(**st.secrets["snowflake"])
+  
+conn = init_connection()
+
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+  with conn.cursor() as cur:
+    cur.execute(query)
+    return cur.fetchall()
+
+
+# Query Snowflake for features and predictions
+data = run_query("SELECT * FROM COMBINED_DATA")
