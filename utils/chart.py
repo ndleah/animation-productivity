a380d6888efdd31dccb61dd723b2a86a8fabe5e7
Leah Nguyen <nduongthucanh@gmail.com>
2022-10-11 00:01:23 +1100

    update

--- utils/chart.py
+++ utils/chart.py
@@ -1,22 +1,23 @@
 import altair as alt
 
-
-def get_chart(data):
+# There are 6 metrics in the final view: 
+# Overheads per productive hour
+def overhead_per_prod(data):
     hover = alt.selection_single(
-        fields=["date"],
+        fields=["DATE"],
         nearest=True,
         on="mouseover",
         empty="none",
     )
 
     lines = (
-        alt.Chart(data, title="Human Efficiency by Departments")
+        alt.Chart(data, title="Overheads per Productive Hour")
         .mark_line()
         .encode(
-            x="WEEK",
-            y="HUMAN_EFFICIENCY",
-            color="DEPARTMENT",
-            strokeDash="DEPARTMENT",
+            x="DATE",
+            y="OVERHEADS_PER_PRODUCTIVE",
+            color="DEPARTMENT_NAME",
+            strokeDash="DEPARTMENT_NAME",
         )
     )
 
@@ -28,12 +29,218 @@ def get_chart(data):
         alt.Chart(data)
         .mark_rule()
         .encode(
-            x="WEEK",
-            y="HUMAN_EFFICIENCY",
+            x="DATE",
+            y="OVERHEADS_PER_PRODUCTIVE",
             opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
             tooltip=[
-                alt.Tooltip("WEEK", title="Date"),
-                alt.Tooltip("HUMAN_EFFICIENCY", title="Price (USD)"),
+                alt.Tooltip("DATE", title="Date"),
+                alt.Tooltip("OVERHEADS_PER_PRODUCTIVE", title="Overhead(s)"),
+            ],
+        )
+        .add_selection(hover)
+    )
+
+    return (lines + points + tooltips).interactive()
+
+# average daily artists
+def avg_daily_artists(data):
+    hover = alt.selection_single(
+        fields=["DATE"],
+        nearest=True,
+        on="mouseover",
+        empty="none",
+    )
+
+    lines = (
+        alt.Chart(data, title="Overheads per Productive Hour")
+        .mark_line()
+        .encode(
+            x="DATE",
+            y="ARTISTS",
+            color="DEPARTMENT_NAME",
+            strokeDash="DEPARTMENT_NAME",
+        )
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = lines.transform_filter(hover).mark_circle(size=65)
+
+    # Draw a rule at the location of the selection
+    tooltips = (
+        alt.Chart(data)
+        .mark_rule()
+        .encode(
+            x="DATE",
+            y="ARTISTS",
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("DATE", title="Date"),
+                alt.Tooltip("ARTISTS", title="Artist(s)"),
+            ],
+        )
+        .add_selection(hover)
+    )
+
+    return (lines + points + tooltips).interactive()
+
+
+# filesize productivity (size of files submitted)
+def filesize_productivity(data):
+    hover = alt.selection_single(
+        fields=["DATE"],
+        nearest=True,
+        on="mouseover",
+        empty="none",
+    )
+
+    lines = (
+        alt.Chart(data, title="Filesize Productivity")
+        .mark_line()
+        .encode(
+            x="DATE",
+            y="FILESIZE_PRODUCTIVITY",
+            color="DEPARTMENT_NAME",
+            strokeDash="DEPARTMENT_NAME",
+        )
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = lines.transform_filter(hover).mark_circle(size=65)
+
+    # Draw a rule at the location of the selection
+    tooltips = (
+        alt.Chart(data)
+        .mark_rule()
+        .encode(
+            x="DATE",
+            y="FILESIZE_PRODUCTIVITY",
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("DATE", title="Date"),
+                alt.Tooltip("FILESIZE_PRODUCTIVITY", title="mb"),
+            ],
+        )
+        .add_selection(hover)
+    )
+
+    return (lines + points + tooltips).interactive()
+
+# count productivity (number of files submitted including duplicate submissions)
+def count_productivity(data):
+    hover = alt.selection_single(
+        fields=["DATE"],
+        nearest=True,
+        on="mouseover",
+        empty="none",
+    )
+
+    lines = (
+        alt.Chart(data, title="Count Productivity")
+        .mark_line()
+        .encode(
+            x="DATE",
+            y="COUNT_PRODUCTIVITY",
+            color="DEPARTMENT_NAME",
+            strokeDash="DEPARTMENT_NAME",
+        )
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = lines.transform_filter(hover).mark_circle(size=65)
+
+    # Draw a rule at the location of the selection
+    tooltips = (
+        alt.Chart(data)
+        .mark_rule()
+        .encode(
+            x="DATE",
+            y="COUNT_PRODUCTIVITY",
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("DATE", title="Date"),
+                alt.Tooltip("COUNT_PRODUCTIVITY", title="File(s)"),
+            ],
+        )
+        .add_selection(hover)
+    )
+
+    return (lines + points + tooltips).interactive()
+
+# review productivity (their old one)
+def review_productivity(data):
+    hover = alt.selection_single(
+        fields=["DATE"],
+        nearest=True,
+        on="mouseover",
+        empty="none",
+    )
+
+    lines = (
+        alt.Chart(data, title="Review Productivity")
+        .mark_line()
+        .encode(
+            x="DATE",
+            y="REVIEW_PRODUCTIVITY",
+            color="DEPARTMENT_NAME",
+            strokeDash="DEPARTMENT_NAME",
+        )
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = lines.transform_filter(hover).mark_circle(size=65)
+
+    # Draw a rule at the location of the selection
+    tooltips = (
+        alt.Chart(data)
+        .mark_rule()
+        .encode(
+            x="DATE",
+            y="REVIEW_PRODUCTIVITY",
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("DATE", title="Date"),
+                alt.Tooltip("REVIEW_PRODUCTIVITY", title="File(s)"),
+            ],
+        )
+        .add_selection(hover)
+    )
+
+    return (lines + points + tooltips).interactive()
+
+# average wait time
+def avg_wait_time(data):
+    hover = alt.selection_single(
+        fields=["DATE"],
+        nearest=True,
+        on="mouseover",
+        empty="none",
+    )
+
+    lines = (
+        alt.Chart(data, title="Review Productivity")
+        .mark_line()
+        .encode(
+            x="DATE",
+            y="AVG_WAITTIME",
+            color="DEPARTMENT_NAME",
+            strokeDash="DEPARTMENT_NAME",
+        )
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = lines.transform_filter(hover).mark_circle(size=65)
+
+    # Draw a rule at the location of the selection
+    tooltips = (
+        alt.Chart(data)
+        .mark_rule()
+        .encode(
+            x="DATE",
+            y="AVG_WAITTIME",
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("DATE", title="Date"),
+                alt.Tooltip("REVIEW_PRODUCTIVITY", title="File(s)"),
             ],
         )
         .add_selection(hover)
--- utils/db.py
+++ utils/db.py
@@ -4,6 +4,7 @@ import snowflake.connector
 import streamlit as st
 import os
 import pandas as pd
+from datetime import date
 
 # from constant import user, password, account, warehouse, database, schema
 
@@ -22,7 +23,27 @@ def run_query(query):
 
 
 # Query Snowflake for features and predictions
-data = run_query("SELECT * FROM COMBINED_DATA")
+data = run_query("SELECT * FROM V_DATA_EXPORT")
 
 # Create dataframe of features and predictions
-df = pd.DataFrame(data, columns=["WEEK", "DEPARTMENT", "WEIGHTED_OUTPUT", "DAYS_LOGGED", "WEIGHTED_OUTPUT_MOD", "DAYS_LOGGED_MOD", "WEIGHTED_OUTPUT_MOVING_AVERAGE", "DAYS_LOGGED_MOD_MOVING_AVERAGE", "HUMAN_EFFICIENCY"])
+df = pd.DataFrame(data, columns=["DATE", "DEPARTMENT_NAME", "PRODUCTIVE_HOURS", "OVERHEAD_HOURS", "TOTAL_HOURS", "OVERHEADS_PER_PRODUCTIVE", "ARTISTS", "FILSESIZE_PRODUCTIVITY", "COUNT_PRODUCTIVITY", "REVIEW_PRODUCTIVITY", "AVG_WAITTIME"])
+
+# using dictionary to convert specific columns
+convert_dict = {'DATE':str,
+                'DEPARTMENT_NAME': str,
+                'PRODUCTIVE_HOURS': int,
+                'OVERHEAD_HOURS': float,
+                'TOTAL_HOURS': float,
+                'OVERHEADS_PER_PRODUCTIVE': float,
+                'ARTISTS': float,
+                'FILSESIZE_PRODUCTIVITY': float,
+                'COUNT_PRODUCTIVITY': float,
+                'REVIEW_PRODUCTIVITY': float,
+                'AVG_WAITTIME': float
+                }
+
+df = df.astype(convert_dict)
+
+# convert the 'Date' column to datetime format
+df['DATE']= pd.to_datetime(df['DATE'])
+
